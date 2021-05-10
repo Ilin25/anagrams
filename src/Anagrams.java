5ce0de661021a65b85f910e268cf7267c0ef88f3
Rustam <rust-ilin@mail.ru>
2021-05-10 14:47:32 +0300

    The application has been moved to a single class.Fixed comments.First edit.

--- src/Anagrams.java
+++ src/Anagrams.java
@@ -1,23 +1,59 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Anagrams {
-    public static void main(String[] args) throws IOException {
-        startProgram();
-    }
 
-    private static void startProgram() throws IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader inputText = new BufferedReader(new InputStreamReader(System.in));
         String text;
         System.out.println("Введи текст ");
         System.out.println("Для выхода жми end ");
-        while ((text = inputText.readLine()) != null){
-            if(text.equals("end")){
+        while ((text = inputText.readLine()) != null) {
+            if (text.equals("end")) {
                 System.out.println("GOOD BYE");
                 break;
             }
-            TextReversal.reverseText(text);
+            System.out.println(flipText(text));
         }
+    }
+
+
+    public static String flipText(String inputText){
+        String[] words = inputText.split(" ");
+        StringBuilder reverseText = new StringBuilder();
+        String oneWord ;
+        String reverseOneWord ;
+
+        for (int i = 0; i < words.length; i++) {
+            oneWord = words[i];
+            reverseOneWord = reverseWord(oneWord);
+            reverseText = reverseText.append(reverseOneWord).append(" ");
+        }
+        return reverseText.toString();
+    }
+
+
+    private static String reverseWord(String oneWord){
+        Map<Integer,Character> initialNonLetterOrder = new HashMap<>();
+        StringBuilder reverseWord = new StringBuilder();
+
+        for (int i = oneWord.length()-1; i >= 0 ; i--) {
+            char symbol = oneWord.charAt(i);
+
+            if (!Character.isLetter(symbol))
+            {
+                initialNonLetterOrder.put(i, symbol);
+                continue;
+            } else
+                reverseWord.append(symbol);
+        }
+
+        initialNonLetterOrder.forEach((position, value)->
+                reverseWord.insert(position,value.toString()));
+
+        return reverseWord.toString();
     }
 }
